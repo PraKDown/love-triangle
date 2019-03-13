e575b42d7b75b710082d62e4e99d797c197c73d9
Vyacheslav Tkachev <dragbarxd@gmail.com>
2019-03-13 18:53:14 +0300

    fix: fix function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    if (preferences[i] == 0) continue;
+    const first = preferences[i];
+    const second = preferences[first-1];
+    if (second-1 == i) continue;
+    const third = preferences[second-1];
+    if(third - 1 == i) {
+			count++;
+      preferences[first-1] = 0
+      preferences[second-1] = 0
+  	}
+	}
+	return count;	
 };
